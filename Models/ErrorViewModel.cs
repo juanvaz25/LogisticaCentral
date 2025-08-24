0ef58663c0cf089f68a0453feecf4962e5e14444
juanvaz25 <jjvazquezin@gmail.com>
2025-08-24 20:01:36 -0300

    Primer commit Proyecto Logistica Central Pampeana

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LogisticaCentral.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
